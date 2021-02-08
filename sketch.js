0e46a75fcab5555dd07c6cf75a1903e9a9c7ea23
anchita93gupta <76733471+anchita93gupta@users.noreply.github.com>
2021-02-08 16:51:28 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,45 +1,32 @@
-var fixedRect, movingRect;
-
+var fixedRect, movingRect,fixedRect1;
 function setup() {
   createCanvas(1200,800);
-  fixedRect = createSprite(600, 400, 50, 80);
-  fixedRect.shapeColor = "green";
-  fixedRect.debug = true;
-  movingRect = createSprite(400,200,80,30);
+  
+ 
+  movingRect = createSprite(800, 400,80,30);
   movingRect.shapeColor = "green";
-  movingRect.debug = true;
+fixedRect= createSprite(100, 100, 50, 50);
+fixedRect.shapeColor = "green";
+fixedRect1 = createSprite(200, 100, 50, 50);
+fixedRect1.shapeColor = "green";
+  
 }
-
 function draw() {
   background(0,0,0);  
   movingRect.x = World.mouseX;
   movingRect.y = World.mouseY;
-
-  
-  if(isTouching())
-  {
-    movingRect.shapeColor = "red";
-fixedRect.shapeColor = "red";
+  if(isTouching(movingRect, fixedRect)){
+    movingRect.shapeColor = "blue";
+    fixedRect.shapeColor = "blue";
   }
-  else
-  {
+  else if(isTouching(movingRect, fixedRect1)){
+    movingRect.shapeColor = "blue";
+    fixedRect1.shapeColor = "blue";
+  }
+  else {
     movingRect.shapeColor = "green";
-fixedRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+    fixedRect1.shapeColor = "green"
   }
   drawSprites();
-}
-
-function isTouching()
-{
-//collision detection algo
-
-if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
-  && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
-  && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
-  && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
-return true;
-}
-else {
-return false;
-}
 }
